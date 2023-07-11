75bdd89f8a8b0c04ac0ae4e16069cd16b82036cb
Jason Ling <jasonling1996@gmail.com>
2023-07-11 15:29:18 -0700

    Added a background to the homepage

--- app/Foot/Footer.js
+++ app/Foot/Footer.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import { Box } from "@chakra-ui/react";
+import React from "react";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <Box backgroundColor={"green.600"}>
+      <Box
+        maxWidth={"1280px"}
+        margin={"0 auto"}
+        paddingY={"3rem"}
+        paddingX={{ base: "2rem", sm: "0" }}
+      ></Box>
+    </Box>
+  );
 }
--- app/page.js
+++ app/page.js
@@ -1,13 +1,52 @@
 "use client"
 import Navbar from "./Nav/Navbar"
 import Footer from "./Foot/Footer";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, Box, Text } from "@chakra-ui/react";
 export default function Home() {
   return (
     <>
     <ChakraProvider>
     <Navbar/>
-   <div>Hello Jason</div>
+    <Box
+    position={"relative"}
+    minHeight={{base: "110vh"}}
+    backgroundPosition={"center"}
+    backgroundSize={"cover"}
+    backgroundAttachment={"fixed"}
+    backgroundImage={"/homepageimage.jpg"}
+    >
+      <Box
+      position={"absolute"}
+      width={"100%"}
+      height={"100%"}
+      opacity={"0.75"}
+      backgroundColor={"green.600"}
+      >
+        <Box
+        display={"flex"}
+        flexDirection={"row"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        maxWidth={"1280px"}
+        position={"absolute"}
+        color={"white"}
+        fontWeight={"medium"}
+        left={"0"}
+        right={"0"}
+        top={"0"}
+        bottom={"0"}
+        margin={"0 auto"}
+        padding={"2rem"}
+        >
+            <Text
+            fontSize={"4xl"}
+            lineHeight={"shorter"}
+            marginBottom={"1.5rem"}>
+              Elevate Your Lifestyle: <br></br> Unravel <strong>Luxury</strong> with Every Listing
+            </Text>
+        </Box>
+      </Box>
+    </Box>
    <Footer/>
    </ChakraProvider>
    </>

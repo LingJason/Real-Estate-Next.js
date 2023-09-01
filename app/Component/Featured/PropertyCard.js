e10c40cc8cd1a475abebd9f2d84fdf24c7db184f
Jason Ling <jasonling1996@gmail.com>
2023-09-01 13:37:03 -0700

    Create basic display for prop info

--- app/Component/Featured/PropertyCard.js
+++ app/Component/Featured/PropertyCard.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import usePropertyFormat from "../Hooks/usePropertyFormat";
+import { Box } from "@chakra-ui/react";
 
-function PropertyCard() {
+function PropertyCard(props) {
+  const {
+    address,
+    coverPhoto,
+    propertyType,
+    price,
+    title,
+    room,
+    bath,
+    purpose,
+    sq,
+    externalID,
+  } = usePropertyFormat(props);
+
   return (
-    <div>PropertyCard</div>
-  )
+    <Box 
+      marginBottom={"4rem"} 
+      backgroundColor={"#fff"}>
+      <Box
+        height={"250px"}
+        backgroundPosition={"center center"}
+        backgroundSize={"cover"}
+        position={"relative"}
+        display={"flex"}
+        flexDirection={"column"}
+        justifyContent={"space-between"}
+      ></Box>
+    </Box>
+  );
 }
 
-export default PropertyCard
+export default PropertyCard;

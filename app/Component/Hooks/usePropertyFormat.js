66c66a4f520955b90409070056ae5855d4b8d01e
Jason Ling <jasonling1996@gmail.com>
2023-09-01 13:26:39 -0700

    Create usePropertyFormat missing prop data

--- app/Component/Hooks/usePropertyFormat.js
+++ app/Component/Hooks/usePropertyFormat.js
@@ -1,10 +1,29 @@
 import React from 'react'
 
 function usePropertyFormat(props) {
-  const address = props.property
-  return (
-    <div>usePropertyFormat</div>
-  )
+  const address = props.property;
+  const coverPhoto = props.property;
+  const propertyType = props.property;
+  const price = props.property;
+  const title = props.property;
+  const room = props.property;
+  const bath = props.property;
+  const purpose = props.property;
+  const sq = props.property;
+  const externalID = props.property;
+
+  return {
+    address,
+    coverPhoto,
+    propertyType,
+    price,
+    title,
+    room,
+    bath,
+    purpose,
+    sq,
+    externalID
+  }
 }
 
 export default usePropertyFormat
